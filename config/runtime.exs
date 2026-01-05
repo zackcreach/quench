ddc7fdf2996aff447e9f0cd4f8f0fcf39492e570
Zack Creach <zackcreach@gmail.com>
2026-01-04 21:41:47 -0500

    server updates

--- config/runtime.exs
+++ config/runtime.exs
@@ -54,6 +54,7 @@ if config_env() == :prod do
       """
 
   host = System.get_env("PHX_HOST") || "example.com"
+  port = String.to_integer(System.get_env("PORT") || "4001")
 
   config :quench, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
 
@@ -64,7 +65,8 @@ if config_env() == :prod do
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
       # See the documentation on https://hexdocs.pm/bandit/Bandit.html#t:options/0
       # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-      ip: {0, 0, 0, 0, 0, 0, 0, 0}
+      ip: {0, 0, 0, 0, 0, 0, 0, 0},
+      port: port
     ],
     secret_key_base: secret_key_base
 
