0316d115b00f84baf4b7984356a31c6c89c6a212
Zack Creach <zackcreach@gmail.com>
2026-06-03 16:35:31 -0400

    config updates for docker deploy

--- config/runtime.exs
+++ config/runtime.exs
@@ -55,11 +55,19 @@ if config_env() == :prod do
 
   host = System.get_env("PHX_HOST") || "example.com"
   port = String.to_integer(System.get_env("PORT") || "4001")
+  scheme = System.get_env("PHX_SCHEME") || "https"
+  url_port = String.to_integer(System.get_env("PHX_PORT") || "443")
+
+  check_origin =
+    "PHX_CHECK_ORIGIN"
+    |> System.get_env("#{scheme}://#{host}:#{url_port}")
+    |> String.split(",", trim: true)
 
   config :quench, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
 
   config :quench, QuenchWeb.Endpoint,
-    url: [host: host, port: 443, scheme: "https"],
+    url: [host: host, port: url_port, scheme: scheme],
+    check_origin: check_origin,
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
