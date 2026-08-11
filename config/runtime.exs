6fc11ec49ba17df1a8651bd7427eaecf903dab4a
Zack Creach <zackcreach@gmail.com>
2026-08-11 16:51:46 -0400

    Support PostgreSQL socket authentication

--- config/runtime.exs
+++ config/runtime.exs
@@ -24,22 +24,34 @@ config :quench, QuenchWeb.Endpoint,
   http: [port: String.to_integer(System.get_env("PORT", "4000"))]
 
 if config_env() == :prod do
-  database_url =
-    System.get_env("DATABASE_URL") ||
-      raise """
-      environment variable DATABASE_URL is missing.
-      For example: ecto://USER:PASS@HOST/DATABASE
-      """
+  database_options =
+    case System.get_env("DATABASE_SOCKET_DIR") do
+      nil ->
+        database_url =
+          System.get_env("DATABASE_URL") ||
+            raise """
+            environment variable DATABASE_URL is missing.
+            For example: ecto://USER:PASS@HOST/DATABASE
+            """
 
-  maybe_ipv6 = if System.get_env("ECTO_IPV6") in ~w(true 1), do: [:inet6], else: []
+        maybe_ipv6 = if System.get_env("ECTO_IPV6") in ~w(true 1), do: [:inet6], else: []
+        [url: database_url, socket_options: maybe_ipv6]
 
-  config :quench, Quench.Repo,
-    # ssl: true,
-    url: database_url,
-    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-    # For machines with several cores, consider starting multiple pools of `pool_size`
-    # pool_count: 4,
-    socket_options: maybe_ipv6
+      socket_dir ->
+        username =
+          System.get_env("DATABASE_USERNAME") ||
+            raise "environment variable DATABASE_USERNAME is required with DATABASE_SOCKET_DIR."
+
+        database =
+          System.get_env("DATABASE_NAME") ||
+            raise "environment variable DATABASE_NAME is required with DATABASE_SOCKET_DIR."
+
+        [socket_dir: socket_dir, username: username, database: database]
+    end
+
+  config :quench,
+         Quench.Repo,
+         [pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")] ++ database_options
 
   # The secret key base is used to sign/encrypt cookies and other secrets.
   # A default value is used in config/dev.exs and config/test.exs but you
