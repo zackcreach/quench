c0ece3b4013746bf0e7c599c1b4c6e35f20afc05
Zack Creach <zackcreach@gmail.com>
2026-08-14 15:45:18 -0400

    disable unavailable Swoosh API client

--- config/prod.exs
+++ config/prod.exs
@@ -3,5 +3,7 @@ import Config
 # Do not print debug messages in production
 config :logger, level: :info
 
+config :swoosh, :api_client, false
+
 # Runtime production configuration, including reading
 # of environment variables, is done on config/runtime.exs.
